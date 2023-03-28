433ec1a211ac9918343b280ffccb39b22ccfcce8
Guilherme Martinelli Taglietti <107369732+GuiTaglietti@users.noreply.github.com>
2023-03-27 22:14:48 -0300

    Delete PHP directory

--- PHP/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASW', '');
-    define('DTBASE', 'testecrud');
-    
-    $con = new MySQLi(HOST, USER, PASW, DTBASE);
-?>
